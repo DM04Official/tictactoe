1c8ba19fd7acb53fcccb4ca430fbe5c736f4edb9
Nathan <Nathan@Lenovo-PC>
2019-11-08 11:01:20 -0500

    more new class

--- TicTacToe/src/ticTacToe/Gameplay.java
+++ TicTacToe/src/ticTacToe/Gameplay.java
@@ -1,5 +1,39 @@
 package ticTacToe;
 
-public class Gameplay {
+import java.awt.event.ActionEvent;
 
+public class Gameplay {
+	Board flat = new Board();
+	
+	public void doGameplay(ActionEvent meow)
+	{
+		if(meow.getSource() == flat.one)
+		{
+			boxClicked(flat.one);
+		} else if(meow.getSource() == flat.two)
+		{
+			boxClicked(flat.two);
+		} else if(meow.getSource() == flat.four)
+		{
+			boxClicked(flat.three);
+		} else if(meow.getSource() == flat.five)
+		{
+			boxClicked(four);
+		} else if(meow.getSource() == flat.six)
+		{
+			boxClicked(five);
+		} else if(meow.getSource() == flat.seven)
+		{
+			boxClicked(six);
+		} else if(meow.getSource()== flat.seven)
+		{
+			boxClicked(seven);
+		} else if(meow.getSource() == flat.seven)
+		{
+			boxClicked(eight);
+		} else if(ouch.getSource() == flat.seven)
+		{
+			boxClicked(nine);
+		}
+	}
 }
